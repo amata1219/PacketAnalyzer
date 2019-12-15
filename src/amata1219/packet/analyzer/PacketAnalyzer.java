fe73d3fb2797cd5a5e557900f4a6003a1d0c7a52
amata1219 <suzukiamata@gmail.com>
2019-12-16 02:20:59 +0900

    こみっとー

--- src/amata1219/packet/analyzer/PacketAnalyzer.java
+++ src/amata1219/packet/analyzer/PacketAnalyzer.java
@@ -2,10 +2,22 @@ package amata1219.packet.analyzer;
 
 import org.bukkit.entity.Player;
 
-public class PacketAnalyzer extends PacketHandler {
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import net.minecraft.server.v1_8_R3.PacketPlayOutKickDisconnect;
 
+public class PacketAnalyzer extends PacketHandler {
+	
 	public PacketAnalyzer(Player player) {
 		super(player);
+		
+		registerOutAnalyzers(
+			Analyzer.of(PacketPlayOutChat.class, p -> {
+				
+			}),
+			Analyzer.of(PacketPlayOutKickDisconnect.class, p -> {
+				
+			})
+		);
 	}
-
+	
 }
