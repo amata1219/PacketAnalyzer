b643741a2e8a4c1727c3ead2648499d365d327e3
amata1219 <suzukiamata@gmail.com>
2019-12-17 16:52:54 +0900

    こみっとー

--- src/amata1219/packet/analyzer/Main.java
+++ src/amata1219/packet/analyzer/Main.java
@@ -1,5 +1,7 @@
 package amata1219.packet.analyzer;
 
+import java.io.IOException;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,6 +10,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import net.minecraft.server.v1_8_R3.PacketDataSerializer;
+import net.minecraft.server.v1_8_R3.PacketPlayOutEntityEffect;
+import net.minecraft.server.v1_8_R3.PacketPlayOutRemoveEntityEffect;
 
 public class Main extends JavaPlugin implements Listener {
 	
@@ -21,7 +29,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable(){
 		plugin = this;
 		
-		getCommand("analyze").setExecutor(this);
+		getCommand("packet").setExecutor(this);
 		
 		getServer().getPluginManager().registerEvents(this, this);
 	}
@@ -33,13 +41,57 @@ public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		if(!(sender instanceof Player)) return true;
+		if(!(sender instanceof Player) || args.length == 0) return true;
 		
 		Player player = (Player) sender;
-		if(PacketInjector.isInjectedTo(player)) PacketInjector.unapplyTo(player);
-		else PacketInjector.applyTo(player, PacketAnalyzer.class);
 		
-		player.sendMessage(PacketInjector.isInjectedTo(player) ? "パケットの解析を開始しました。" : "パケットの解析を終了しました。");
+		switch(args[0]){
+		  case "analyze": {
+			if(PacketInjector.isInjectedTo(player)) PacketInjector.unapplyTo(player);
+			else PacketInjector.applyTo(player, PacketAnalyzer.class);
+			player.sendMessage(PacketInjector.isInjectedTo(player) ? "パケットの解析を開始しました。" : "パケットの解析を終了しました。");
+			break;
+		} case "apply": {
+			ByteBuf buffer = Unpooled.buffer();
+			buffer.writeInt(PacketInjector.extractEntityPlayer(player).getId());
+			buffer.writeByte(14);
+			buffer.writeByte(0);
+			buffer.writeInt(1728000);
+			buffer.writeByte(2);
+			PacketDataSerializer data = new PacketDataSerializer(buffer);
+			PacketPlayOutEntityEffect packet = new PacketPlayOutEntityEffect();
+			try {
+				packet.a(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			for(Player other : getServer().getOnlinePlayers()) if(!other.equals(player))
+				PacketInjector.extractEntityPlayer(other).playerConnection.sendPacket(packet);
+			
+			player.sendMessage("他プレイヤーを透明化しました。");
+			break;
+		} case "unapply": {
+			ByteBuf buffer = Unpooled.buffer();
+			buffer.writeInt(PacketInjector.extractEntityPlayer(player).getId());
+			buffer.writeByte(14);
+			PacketDataSerializer data = new PacketDataSerializer(buffer);
+			PacketPlayOutRemoveEntityEffect packet = new PacketPlayOutRemoveEntityEffect();
+			try {
+				packet.a(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			for(Player other : getServer().getOnlinePlayers()) if(!other.equals(player))
+				PacketInjector.extractEntityPlayer(other).playerConnection.sendPacket(packet);
+			
+			player.sendMessage("他プレイヤーの透明化を解除しました。");
+			break;
+		} default:
+			break;
+		}
+		
 		return true;
 	}
 	
@@ -52,5 +104,5 @@ public class Main extends JavaPlugin implements Listener {
 	public void onQuit(PlayerQuitEvent event){
 		PacketInjector.unapplyTo(event.getPlayer());
 	}
-
+	
 }
